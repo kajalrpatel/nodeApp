04a39763cac359b04114f766103e2725e1d5dc99
Kajal Patel <39385547+kajalrpatel@users.noreply.github.com>
2020-01-10 20:32:37 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl' : 'mongodb://localhost:27017/nodedb',
     'facebook': {
-        clientId: '757116304808823',
-        clientSecret: '50479832c449a9fd61bb4bfb38f82641'
+        clientId: 'yourclient id',
+        clientSecret: 'secret key'
     }
 }
